369f77e030703fad0e8be6fe4fb5acc6716d5391
Anusorn Kanchanangkun <scada.josh@gmail.com>
2024-12-02 23:30:42 +0700

    finished - day1

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,14 @@
+// import AppAlert from "../components/AppAlert"
+// import AppAlert from "@/app/components/AppAlert"
+import Alert from "@mui/material/Alert";
+import CheckIcon from '@mui/icons-material/Check';
+
 export default function Page() {
   return (
     <main>
+      <Alert icon={<CheckIcon fontSize="inherit" />} severity="success">
+      Here is a gentle confirmation that your action was successful.
+    </Alert>
       <p>About Page</p>
     </main>
   );
---
+++ src/app/components/AppAlert.tsx
@@ -0,0 +1,7 @@
+export default function AppAlert() {
+  return (
+    <div style={{ backgroundColor: 'greenyellow'}}>
+        AppAlert
+    </div>
+  )
+}
